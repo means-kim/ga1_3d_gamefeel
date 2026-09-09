3edd58ee6bc5db0b72f63aac330837c780235b15
김민수 <kms99400@gmail.com>
2026-09-09 18:56:46 +0900

    ✨ feat: Hit시 이펙트 추가

--- Assets/02.Scripts/Weapon/WeaponHitBox.cs
+++ Assets/02.Scripts/Weapon/WeaponHitBox.cs
@@ -5,16 +5,23 @@ public class WeaponHitBox : MonoBehaviour
     [SerializeField] private int _damage;
     [SerializeField] private PlayerAttack _playerAttack;
     [SerializeField] private CameraShake _cameraShake;
+    [SerializeField] private GameObject _hitEffect;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
             Enemy enemy = other.GetComponent<Enemy>();
+            SpwanHitEffect();
             enemy.TakeDamage(_damage);
             Debug.Log("충동직전");
             _playerAttack.StartRecovery();
             _cameraShake.ScreenShake();
         }
+    }
+
+    private void SpwanHitEffect()
+    {
+        Instantiate(_hitEffect, transform.position, Quaternion.identity);
     }
 }
