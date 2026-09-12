d67f13b5991d2f946bf8900650f6b6caebd4e6b5
means-kim <kms99400@gmail.com>
2026-09-12 18:22:20 +0900

    ✨ feat: 마우스 위치에 따른 카메라 회전 추가

--- Assets/02.Scripts/Camera/CameraFollow.cs
+++ Assets/02.Scripts/Camera/CameraFollow.cs
@@ -4,10 +4,26 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _mouseSensitivity = 5f;
+
+    private float _valueY;
 
     private void LateUpdate()
     {
-        transform.position = _player.position + _offset;
+        PlayerFollow();
+        RotateCamera();
+    }
 
+    private void PlayerFollow()
+    {
+        transform.position = _player.position + _offset;
+    }
+
+    private void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        _valueY += mouseX * _mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(0f, _valueY, 0f);
     }
 }
